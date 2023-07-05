c6f8dae189f23ab0e8cb772e04cd6105e63b741b
Ephraim <712020:b306d4cd-34ab-4c03-a883-027b3dc54286@bots.bitbucket.org>
2023-07-05 17:15:54 +0200

    last commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,9 +11,6 @@ const LoginPage = () => {
                 8,
                 'password must contain 8 or more characters with at least one of each: uppercase, lowercase, number and special'
             )
-          .minUppercase(1, 'password must contain at least 1 upper case letter')
-          .minNumbers(1, 'password must contain at least 1 number')
-          .minSymbols(1, 'password must contain at least 1 special character')
         });
 
   const formik = useFormik({
