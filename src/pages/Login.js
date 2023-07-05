0470d9e1f00079d04ee8572edbb8d79e9b6535c2
Ephraim <712020:b306d4cd-34ab-4c03-a883-027b3dc54286@bots.bitbucket.org>
2023-07-05 17:00:49 +0200

    Question 7 Validation

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const LoginPage = () => {
+    const LoginSchema = Yup.object().shape({
+        username: Yup.string()
+            .required('Required'),
+        password: Yup.string()
+            .min(
+                8,
+                'password must contain 8 or more characters with at least one of each: uppercase, lowercase, number and special'
+            )
+          .minLowercase(1, 'password must contain at least 1 lower case letter')
+          .minUppercase(1, 'password must contain at least 1 upper case letter')
+          .minNumbers(1, 'password must contain at least 1 number')
+          .minSymbols(1, 'password must contain at least 1 special character')
+        });
+
   const formik = useFormik({
     initialValues: {
       username: '',
       password: ''
     },
+    validationSchema: {LoginSchema},
     onSubmit: values => {
         console.log(values);
     },
@@ -40,7 +56,7 @@ const LoginPage = () => {
                     />
                 </div>
             </div>
-        <button type="submit" className="button is-primary">Submit</button>
+        <button type="submit" disabled={!(formik.isValid && formik.dirty)} className="button is-primary">Submit</button>
         </form>
       </div>
   );
